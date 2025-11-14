12d502c99ca440d8fc724f2ae3455685e656c04f
vadde sai kiran <vaddesaikiran8@gmail.com>
2025-11-14 11:58:44 +0530

    Initial Jenkins POC

---
+++ main.py
@@ -0,0 +1,5 @@
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Result:", multiply(5, 10))
---
+++ test_main.py
@@ -0,0 +1,4 @@
+from main import multiply
+
+def test_multiply():
+    assert multiply(2, 3) == 6
